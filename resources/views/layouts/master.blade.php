43cc8d4fceb1695533ea73633836f602ee432755
shizler <win99nie@gmail.com>
2020-08-03 12:56:47 +0300

    heroku

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,39 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <title>Kinyia Groceries</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    
-    <link href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,500,600,700,800&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Lora:400,400i,700,700i&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Amatic+SC:400,700&display=swap" rel="stylesheet">
-
-    <!-- <link rel="stylesheet" href="{{ asset('css/open-iconic-bootstrap.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/animate.css') }}">
-    
-    <link rel="stylesheet" href="{{ asset('css/owl.carousel.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/owl.theme.default.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/magnific-popup.css') }}">
-
-    <link rel="stylesheet" href="{{ asset('css/aos.css') }}">
-
-    
-
-    <link rel="stylesheet" href="{{ asset('css/bootstrap-datepicker.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/jquery.timepicker.css') }}">
-
-    
-    <link rel="stylesheet" href="{{ asset('css/flaticon.css') }}">
-    
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}"> -->
-    <!-- <link rel="stylesheet" href="{{ asset('css/ionicons.min.css') }}"> -->
-    <link rel="stylesheet" href="{{ asset('css/icomoon.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/main.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/navbar.css') }}">
+    @include('includes.head')
   </head>
   <body class="goto-here">
-	@extends('layouts.header')
-
+    <div class="">
+      <header>
+      @include('includes.header')
+      </header>
+      <div>
+        @yield('content')
+      </div>
+      <footer>
+      <!-- @include('includes.footer') -->
+      </footer>
+    </div>
   </body>
 </html>
